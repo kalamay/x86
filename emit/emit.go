6131300230c8ea3c7934f7ff1bef64be9159b3fa
Jeremy Larkin <jeremylarkin@gmail.com>
2021-09-01 12:59:28 -0700

    update emit

--- emit/emit.go
+++ emit/emit.go
@@ -3,8 +3,8 @@ package emit
 import (
 	"io"
 
-	"github.com/kalamay/x86/asm/amd64"
-	"github.com/kalamay/x86/asm/amd64/inst"
+	. "github.com/kalamay/x86/asm/amd64"
+	. "github.com/kalamay/x86/asm/amd64/inst"
 )
 
 func New(w io.Writer) *Emitter {
@@ -17,9 +17,9 @@ type Emitter struct {
 	w io.Writer
 }
 
-func (e *Emitter) MOV(ops ...amd64.Op) (n int, err error) {
+func (e *Emitter) Emit(in *InstSet, ops []Op) (n int, err error) {
 	b := [15]byte{}
-	if n, err = inst.MOV.Encode(b[:], ops); err != nil {
+	if n, err = in.Encode(b[:], ops); err != nil {
 		n = 0
 		e.Errors = append(e.Errors, err)
 	} else if len(e.Errors) == 0 {
@@ -27,3 +27,6 @@ func (e *Emitter) MOV(ops ...amd64.Op) (n int, err error) {
 	}
 	return
 }
+
+func (e *Emitter) ADD(ops ...Op) (n int, err error) { return e.Emit(ADD, ops) }
+func (e *Emitter) MOV(ops ...Op) (n int, err error) { return e.Emit(MOV, ops) }
